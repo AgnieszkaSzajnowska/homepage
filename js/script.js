a8f87ea91e71307090ac5663e17175c66a484048
Agnieszka Szajnowska <agnieszka.badyla@o2.pl>
2025-03-19 10:15:40 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".js-themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+});
