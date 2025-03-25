11a879da2124cb57113de7d95a21492bde7e1aa9
Agnieszka Szajnowska <agnieszka.badyla@o2.pl>
2025-03-25 07:24:16 +0100

    js updated- welcome remove

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 {
-const welcome = () => {
-    console.log("Hello");
-}
+    const welcome = () => {
+        console.log("Hello");
+    }
 
-const ChangeBackgroundClick = document.querySelector(".js-button");
-const body = document.querySelector(".js-body");
-const themeName = document.querySelector(".js-themeName");
+    const ChangeBackgroundClick = document.querySelector(".js-button");
+    const body = document.querySelector(".js-body");
+    const themeName = document.querySelector(".js-themeName");
 
 
-const onChangeBackgroundClick = () => {
-    body.classList.toggle("dark");
-    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
-}
+    const onChangeBackgroundClick = () => {
+        body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    }
 
-const init = () => {
-    ChangeBackgroundClick.addEventListener("click", onChangeBackgroundClick);
-}
- init();
+    const init = () => {
+        ChangeBackgroundClick.addEventListener("click", onChangeBackgroundClick);
+    }
+    init();
 }
